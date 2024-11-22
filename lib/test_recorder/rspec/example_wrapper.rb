bac633f81e70fdb3cc0f5c13ba79fcf9434e1aa8
Jonathan Siegel <248302+usiegj00@users.noreply.github.com>
2024-11-22 13:31:15 +0900

    Update example_wrapper.rb

--- lib/test_recorder/rspec/example_wrapper.rb
+++ lib/test_recorder/rspec/example_wrapper.rb
@@ -3,10 +3,14 @@ module TestRecorder
     module ExampleWrapper
       def run_before_example
         super
-        TestRecorder::RSpec.cdp_recorder.start(
-          page: @example_group_instance.page,
-          enabled: self.metadata[:test_recorder]
-        )
+        if @example_group_instance.respond_to?(:page)
+          TestRecorder::RSpec.cdp_recorder.start(
+            page: @example_group_instance.page,
+            enabled: self.metadata[:test_recorder]
+          )
+        else
+          puts "Test type does not respond to page. Skipping test screen recording."
+        end
       end
     end
   end
