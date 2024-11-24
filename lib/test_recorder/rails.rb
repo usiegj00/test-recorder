1064a5634539c28259b8ea27d99ae156ce610321
Jonathan Siegel <248302+usiegj00@users.noreply.github.com>
2024-11-24 12:17:21 +0900

    Added require for click.

--- lib/test_recorder/rails.rb
+++ lib/test_recorder/rails.rb
@@ -1,5 +1,6 @@
 require "test_recorder/cdp_recorder"
 require "test_recorder/human_fill_in"
+require "test_recorder/human_click"
 require "test_recorder/rails/setup_and_teardown"
 
 ActiveSupport.on_load(:action_dispatch_system_test_case) do
