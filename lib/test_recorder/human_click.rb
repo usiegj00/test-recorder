37350a329af0ea1f04497ef2b48d1f42326ba694
Jonathan Siegel <248302+usiegj00@users.noreply.github.com>
2024-11-28 23:06:41 +0900

    Attempting to display mouse pointer.

--- lib/test_recorder/human_click.rb
+++ lib/test_recorder/human_click.rb
@@ -10,6 +10,86 @@ if defined?(Capybara::Session)
         timestamp = format("%0.08f", timestamp.to_f)
         frame_path = @frames_dir.join("#{filename}_#{timestamp}.png")
       end
+      
+      def add_mouse_pointer
+        javascript = <<~JAVASCRIPT
+          // This injects a box into the page that moves with the mouse;
+          // Useful for debugging
+          function installMouseHelper() {
+            // Install mouse helper only for top-level frame.
+            if (window !== window.parent)
+              return;
+            window.addEventListener('DOMContentLoaded', () => {
+              const box = document.createElement('puppeteer-mouse-pointer');
+              const styleElement = document.createElement('style');
+              styleElement.innerHTML = `
+                puppeteer-mouse-pointer {
+                  pointer-events: none;
+                  position: absolute;
+                  top: 0;
+                  z-index: 10000;
+                  left: 0;
+                  width: 20px;
+                  height: 20px;
+                  background: rgba(0,0,0,.4);
+                  border: 1px solid white;
+                  border-radius: 10px;
+                  margin: -10px 0 0 -10px;
+                  padding: 0;
+                  transition: background .2s, border-radius .2s, border-color .2s;
+                }
+                puppeteer-mouse-pointer.button-1 {
+                  transition: none;
+                  background: rgba(0,0,0,0.9);
+                }
+                puppeteer-mouse-pointer.button-2 {
+                  transition: none;
+                  border-color: rgba(0,0,255,0.9);
+                }
+                puppeteer-mouse-pointer.button-3 {
+                  transition: none;
+                  border-radius: 4px;
+                }
+                puppeteer-mouse-pointer.button-4 {
+                  transition: none;
+                  border-color: rgba(255,0,0,0.9);
+                }
+                puppeteer-mouse-pointer.button-5 {
+                  transition: none;
+                  border-color: rgba(0,255,0,0.9);
+                }
+              `;
+              document.head.appendChild(styleElement);
+              document.body.appendChild(box);
+              document.addEventListener('mousemove', event => {
+                box.style.left = event.pageX + 'px';
+                box.style.top = event.pageY + 'px';
+                updateButtons(event.buttons);
+              }, true);
+              document.addEventListener('mousedown', event => {
+                updateButtons(event.buttons);
+                box.classList.add('button-' + event.which);
+              }, true);
+              document.addEventListener('mouseup', event => {
+                updateButtons(event.buttons);
+                box.classList.remove('button-' + event.which);
+              }, true);
+              function updateButtons(buttons) {
+                for (let i = 0; i < 5; i++)
+                  box.classList.toggle('button-' + i, buttons & (1 << i));
+              }
+            }, false);
+          };
+        installMouseHelper();
+        JAVASCRIPT
+
+        # Define the function on the page
+        self.session.driver.browser.page.evaluate(javascript)
+
+        # Call the function
+        # @page.driver.browser.page.evaluate("installMouseHelper()")
+      end
+
 
       def click(*options, bypass: false)
         # Bypass the human typing simulation
@@ -38,6 +118,7 @@ if defined?(Capybara::Session)
           
           # Move the mouse to the center of the element
           # self.session.driver.browser.mouse.move_to(coords[:x] + coords[:w] / 2, coords[:y] + coords[:h] / 2)
+          add_mouse_pointer
           # simulate_mouse_movement(coords[:x] + coords[:w] / 2, coords[:y] + coords[:h] / 2)
           self.session.driver.browser.mouse.move(x: coords[:x] + coords[:w] / 2, y: coords[:y] + coords[:h] / 2)
           # Click the element
